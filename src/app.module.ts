d937b7816b93313202fa8adaace13acb49ed0f52
Nasiphe Dipa <dipa@thedigitalacademy.co.za>
2021-09-15 11:41:48 +0200

    added backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { PhonebookModule } from './phonebook/phonebook.module';
 
 @Module({
 	imports: [
-		MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),
+		MongooseModule.forRoot("mongodb+srv://Nasiphe:12345@cluster0.9cx3a.mongodb.net/Phonebook?retryWrites=true&w=majority", { autoCreate: true }),
 		PhonebookModule,
 	],
 	controllers: [],
 	providers: [],
 })
 export class AppModule {}
+
+//mongodb+srv://Nasiphe:12345@cluster0.9cx3a.mongodb.net/Phonebook?retryWrites=true&w=majority
